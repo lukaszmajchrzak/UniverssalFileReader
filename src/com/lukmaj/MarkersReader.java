52d3fcedcf3b6c946c8d5c25f98ad9971bb94c55
LUKMAJ <lucas.majchrzak@gmail.com>
2020-03-08 16:25:16 +0100

    Fixed: minor bug

--- src/com/lukmaj/MarkersReader.java
+++ src/com/lukmaj/MarkersReader.java
@@ -1,11 +1,5 @@
 package com.lukmaj;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.util.Scanner;
 
